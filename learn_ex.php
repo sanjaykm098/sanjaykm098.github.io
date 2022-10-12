649ebd99b813dbcddcbe0b57a4f8c7f25af326e4
sanjaykm098 <sounkumar45673@gmail.com>
2022-10-12 10:49:49 +0530

    bj

---
+++ learn_ex.php
@@ -0,0 +1,411 @@
+<html>
+	<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title></title>
+		<style>
+		body
+			{
+				margin:auto;
+			}
+			.main_nav
+				{
+					height:50px;
+					width:100%;
+					background:#195a9a;
+					
+				}
+				.main_header
+				{
+					height:300px;
+					width:100%;
+					background:#195a9a;
+				}
+			.container
+				{
+					height:1200px;
+					width:960px;
+					background:;
+					margin:auto;
+				}
+			.nav
+				{
+					height:50px;
+					width:960px;
+					background:#185c9c;
+					margin:auto;
+					
+				}
+			.sub_nav
+				{
+					height:35px;
+					width:190px;
+					float:left;
+					background:#185c9c;
+					color:white;
+					line-height:35px;
+					text-align:center;
+					border-right:1px solid white; margin-top:10px;
+				}
+			.header
+				{
+					height:100px;
+					width:100%;
+					background:#1d68a7;	
+				}
+			.logo
+				{
+					height:100px;
+					width:300px;
+					background:#1d68a7;
+					margin:auto;
+					text-align:center;
+					color:white;
+				}
+			.content
+				{
+					height:1100px;
+					width:960px;
+					background:white;
+				}
+			.slider
+				{
+					height:300px;
+					width:960px;
+					background:white;
+					border:1px solid gray;
+					
+				}
+			.sub_slider
+				{
+					height:250px;
+					width:960px;
+					background:white;
+					margin-top:20px;
+				}
+			.box
+				{
+					height:250px;
+					width:230px;
+					background:white;
+					float:left;
+					margin-left:10px;
+
+				}
+			.photo
+				{
+					height:120px;
+					width:190px;
+					
+				}
+				input[type="button"]
+				{
+					height:30px;
+					width:80px;
+					margin-left:50px;
+					background:#a3a3a3;
+					border:1px solid #a3a3a3;
+				}
+			
+			.sub_content
+				{
+					height:530px;
+					width:960px;
+					background:white;
+				}
+			.left
+				{
+					height:500px;
+					width:300px;
+					background:white;
+					float:left;
+					margin-top:20px;
+					
+				}
+			.group
+				{
+					height:500px;
+					width:320px;
+					background:white;
+					float:left;
+					margin-left:20px;
+					margin-top:20px;
+					
+				}
+			.right
+				{
+					height:500px;
+					width:300px;
+					background:white;
+					float:left;
+					margin-left:20px;
+					margin-top:20px;
+				}
+			.main_footer
+				{
+					height:50px;
+					width:100%;
+					background:#105084;
+				}
+			.footer
+				{
+					height:50px;
+					width:950px;
+					
+				}
+			.sub_footer
+			{
+				height:50px;
+				width:650px;
+				background:#105084;
+					line-height:50px;
+					color:white;
+					margin:auto;
+				
+			}
+		@media screen and (min-width:240px) and (max-width:940px)
+				{
+				.main_nav
+				{
+					height:auto;
+					width:100%;
+					background:#195a9a;
+					
+				}
+				.nav
+				{
+					height:auto;
+					width:100%;
+					background:#185c9c;
+					
+					
+				}
+			.sub_nav
+				{
+					height:auto;
+					width:100%;
+					float:none;
+					background:#185c9c;
+					color:white;
+					line-height:35px;
+					text-align:center;
+					border-right:0px solid white;
+					border-bottom:1px solid white;
+					margin-top:0px;
+				}
+				.main_header
+				{
+					height:auto;
+					width:100%;
+					background:#195a9a;
+				}
+				.container
+				{
+					height:auto;
+					width:100%;
+					background:;
+					margin:auto;
+				}
+				.header
+				{
+					height:auto;
+					width:100%;
+					background:#1d68a7;	
+				}
+			.logo
+				{
+					height:auto;
+					width:100%;
+					background:#1d68a7;
+				}
+			.content
+				{
+					height:auto;
+					width:100%;
+					background:white;
+				}
+			.slider
+				{
+					height:auto;
+					width:100%;
+					background:white;
+					border:none;
+					
+				}.slider img
+				{
+					height:auto;
+					width:100%;
+					background:white;
+					border:none;
+					
+				}
+			.sub_slider
+				{
+					height:auto;
+					width:100%;
+					background:white;
+					margin-top:20px;
+				}
+				.box
+				{
+					height:auto;
+					width:100%;
+					background:white;
+					margin:auto;
+					float:none;
+					text-align:center;
+					
+				}
+				.photo
+				{
+					margin:auto;					
+				}
+				.photo img
+				{
+					height:auto;
+					width:100%;
+					border:none;
+					
+				}
+				.sub_content
+				{
+					height:auto;
+					width:100%;
+					background:white;
+					
+				}
+			.left
+				{
+					
+					background:white;
+					float:none;
+					
+					
+					margin:auto;
+				}
+			.group
+				{
+					
+					background:white;
+					float:none;
+					margin:auto;
+					
+				}
+			.right
+				{
+					
+					background:white;
+					float:none;
+					
+					margin:auto;
+				}
+			.main_footer
+				{
+					height:auto;
+					width:100%;
+				}
+			.footer
+				{
+					height:auto;
+					width:100%;
+					float:none;
+					
+				}
+				.sub_footer
+			{
+				height:auto;
+				width:100%;
+				background:#105084;
+				line-height:50px;
+				color:white;
+				margin-top:10px;
+				text-align:center;
+				
+			}
+				}
+		</style>
+	</head>
+	<body>
+		<div class="main_nav">
+			<div class="nav">		
+				<div class="sub_nav">ABOUT US</div>
+					<div class="sub_nav">OUR PROGRAMS</div>
+					<div class="sub_nav">OUR STAFF</div>
+					<div class="sub_nav">BLOG</div>
+					<div class="sub_nav">CONTACTS</div>
+			</div>
+		</div>
+		<div class="main_header">
+			<div class="container">
+				<div class="header">
+					<div class="logo">
+						<b style="font-family:Brush script MT; font-size:50px; ">Learnex</b>
+						<pre>EDUCATION CENTER</pre>
+					</div>
+				</div>
+				<div class="content">
+					<div class="slider">
+						<img src="images/book.jpg" height="300" width="960">
+					</div>
+					<div class="sub_slider">
+						<div class="box">
+							<div class="photo"><img src="images/1.jpg" height="120" width="190"></div>
+							<h3>MIASERA LASEA<h3>
+							<input type="button" value="Read More">
+						</div>
+						<div class="box">
+							<div class="photo"><img src="images/degree.jpg" height="120" width="190"></div>
+							<h3>KERYS VERAD<h3>
+							<input type="button" value="Read More">
+						</div>
+						<div class="box">
+							<div class="photo"><img src="images/laptop.png" height="120" width="190"></div>
+							<h3>BERTYA SEDA<h3>
+							<input type="button" value="Read More">
+						</div>
+						<div class="box">
+							<div class="photo"><img src="images/books.jpg" height="120" width="190"></div>
+							<h3>FERODE NAESE<h3>
+							<input type="button" value="Read More">
+						</div>
+					</div>
+					<div class="sub_content">
+						<div class="left">
+							<h3 style="font-family:Brush script MT; color:blue;">NEWS & EVENT</h3>
+							<p><b>14/08/2012</b></p>
+							<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney</p>
+							<br>
+							<hr><br>
+							<p><b>12/05/2012</b></p>
+							<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words</p>
+							<input type="button" value="Read More">
+						</div>
+						<div class="group">
+							<img src="images/group.jpg" height="200" width="320">
+							<br><br>
+							<h3 style="font-family:Brush script MT; color:blue;">WELCOME<i>!</i></h3>
+							<h4>Lorem Ipsum is not simply random</h4>
+							<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							<input type="button" value="Read More">
+						</div>
+						<div class="right">
+							<h3 style="font-family:Brush script MT; color:blue;">MOST POPULAR</h3>
+							<h4 style="color:red;">Prsesent vespibulum</h4>
+							<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+							<hr>
+							<h4  style="color:red;">Aerean Sector</h4>
+							<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+							<hr>
+							<h4  style="color:red;">Aerean Sector</h4>
+							<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="main_footer">
+				<div class="footer">
+						<div class="sub_footer">LEARNEX &copy 2012 | PRIVACY POLICY </div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
